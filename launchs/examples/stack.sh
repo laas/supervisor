fc7479effd54d2de6ef599e89e3f04b5def61247
sdevin <sdevin@laas.fr>
2016-04-18 14:25:33 +0200

    [launchs] add area manager to scripts

--- launchs/examples/stack.sh
+++ launchs/examples/stack.sh
@@ -29,7 +29,35 @@ rosservice call /toaster_simu/set_entity_pose "{id: 'rightHand', ownerId: 'HERAK
 rosservice call /toaster_simu/add_entity "{id: 'base', name: 'base', type: 'joint', ownerId: 'HERAKLES_HUMAN1'}"
 rosservice call /toaster_simu/set_entity_pose "{id: 'base', ownerId: 'HERAKLES_HUMAN1', type: 'joint', x: 6.0, y: 4.1, z: 0.0, roll: 0.0, pitch: 0.0, yaw: 3.14}"
 
+rosservice call /toaster_simu/add_entity "{id: 'head', name: 'head', type: 'joint', ownerId: 'HERAKLES_HUMAN1'}"
+rosservice call /toaster_simu/set_entity_pose "{id: 'head', ownerId: 'HERAKLES_HUMAN1', type: 'joint', x: 6.0, y: 4.1, z: 1.5, roll: 0.0, pitch: 0.0, yaw: 3.14}"
 
+rosservice call /agent_monitor/monitor_all_agents "monitorAll: true"
+
+rosservice call /agent_monitor/add_joint_to_agent "{jointName: 'base', agentId: 'HERAKLES_HUMAN1'}"
+rosservice call /agent_monitor/add_joint_to_agent "{jointName: 'head', agentId: 'HERAKLES_HUMAN1'}"
+rosservice call /agent_monitor/add_joint_to_agent "{jointName: 'rightHand', agentId: 'HERAKLES_HUMAN1'}"
+
+rosservice call /agent_monitor/add_joint_to_agent "{jointName: 'laser_tilt_mount_link', agentId: 'pr2'}"
+
+rosservice call /area_manager/add_area "myArea:
+  id: 0
+  name: 'interaction'
+  myOwner: 'pr2'
+  areaType: ''
+  factType: 'interaction'
+  entityType: 'entities'
+  isCircle: false
+  center: {x: 0.0, y: 0.0, z: 0.0}
+  ray: 2.0
+  poly:
+    points:
+    - {x: 0, y: -1, z: 0}
+    - {x: 2, y: -2, z: 0}
+    - {x: 2, y: 2, z: 0}
+    - {x: 0, y: 1, z: 0}
+    - {x: 0, y: -1, z: 0}
+  insideEntities: [0]"
 
 
 rosservice call /database/add_fact_to_agent "{fact: {property: 'isReachableBy', propertyType: 'state', subProperty: '', subjectId: 'RED_CUBE', targetId: 'PR2_ROBOT', subjectOwnerId: '', targetOwnerId: '', valueType: false, factObservability: 0.0, doubleValue: 0.0, stringValue: '', confidence: 0.0, time: 0, timeStart: 0, timeEnd: 0}, agentId: 'PR2_ROBOT'}"
