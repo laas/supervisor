ecd711dda5adbfcb943907472fa01de256211d40
sdevin <sdevin@laas.fr>
2016-07-10 17:22:07 +0200

    [action_executor] add light control and moveTo for scan

--- action_executor/src/Actions/scan.cpp
+++ action_executor/src/Actions/scan.cpp
@@ -6,6 +6,7 @@ Class allowing the execution of a place action
 **/
 
 #include "action_executor/Actions/scan.h"
+#include "action_executor/Actions/moveTo.h"
 
 Scan::Scan(supervisor_msgs::Action action, Connector* connector) : VirtualAction(connector){
     if(action.parameters.size() == 1){
@@ -38,11 +39,44 @@ bool Scan::plan(){
 
 bool Scan::exec(Server* action_server){
 
-    //TODO: add moveToManip
+    //We go to rest pose if needed
+    if(connector_->rightArmPose_ != connector_->rightArmRestPose_){
+        supervisor_msgs::Action mvToAction;
+        mvToAction.name = "moveTo";
+        mvToAction.actors.push_back(robotName_);
+        mvToAction.parameters.push_back(connector_->rightArmRestPose_);
+
+        MoveTo mvToServer(mvToAction, connector_);
+        if(mvToServer.preconditions()){
+            if(mvToServer.plan()){
+                if(mvToServer.exec(action_server)){
+                    mvToServer.post();
+                }
+            }
+        }
+
+    }
+    if(connector_->leftArmPose_ != connector_->leftArmRestPose_){
+        supervisor_msgs::Action mvToAction;
+        mvToAction.name = "moveTo";
+        mvToAction.actors.push_back(robotName_);
+        mvToAction.parameters.push_back(connector_->leftArmRestPose_);
+
+        MoveTo mvToServer(mvToAction, connector_);
+        if(mvToServer.preconditions()){
+            if(mvToServer.plan()){
+                if(mvToServer.exec(action_server)){
+                    mvToServer.post();
+                }
+            }
+        }
+
+    }
+
 
     //TODO: add checking of head focus
     if(!connector_->simu_){
-        //TODO: start red light
+        controlRobotLight(true);
     }
 
     start_ = clock();
@@ -50,14 +84,14 @@ bool Scan::exec(Server* action_server){
     while(duration < timeScan_){
         if(action_server->isPreemptRequested() || connector_->stopOrder_){
             if(!connector_->simu_){
-                //TODO: stop red light
+                controlRobotLight(false);
             }
             return false;
         }
         duration = (clock() - start_ ) / (double) CLOCKS_PER_SEC;
     }
     if(!connector_->simu_){
-        //TODO: stop red light
+        controlRobotLight(false);
     }
 
     return true;
@@ -67,4 +101,23 @@ bool Scan::exec(Server* action_server){
 bool Scan::post(){
 
 	return true;
+}
+
+void Scan::controlRobotLight(bool on){
+
+    dynamic_reconfigure::ReconfigureRequest srv_req;
+    dynamic_reconfigure::ReconfigureResponse srv_resp;
+    dynamic_reconfigure::IntParameter int_param;
+    dynamic_reconfigure::Config conf;
+
+    int_param.name = "projector_mode";
+    if(on){
+        int_param.value = 3;
+    }else{
+        int_param.value = 1;
+    }
+    conf.ints.push_back(int_param);
+    srv_req.config = conf;
+
+    ros::service::call("/camera_synchronizer_node/set_parameters", srv_req, srv_resp);
 }
